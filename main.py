8adab58b728508815d04e58600b463d4b3cea2fa
Your Name <your-email@example.com>
2026-02-16 06:52:49 +0000

    Fixed library setup

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 
-# S.A.R.A. Project Version 1.1
 class SaraApp(App):
     def build(self):
-        return Button(text='Hello S.A.R.A. is Online!')
+        # একটি সুন্দর বাটন তৈরি করছি
+        return Button(
+            text='Hello Anif! S.A.R.A. is Running...',
+            font_size='20sp',
+            background_color=(0, 0.5, 1, 1)
+        )
 
 if __name__ == '__main__':
     SaraApp().run()
