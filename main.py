6036e5c53472e3de1c66affa80d422ebab2d09f1
Your Name <your-email@example.com>
2026-02-16 07:04:36 +0000

    Fixed typo from inport to import

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.uix.button import Button
 
 class SaraApp(App):
     def build(self):
-        return Button(text='Hello Anif! S.A.R.A. is Running...')
+        return Button(text='Hello Anif! S.A.R.A. is Online!')
 
 if __name__ == '__main__':
     SaraApp().run()
