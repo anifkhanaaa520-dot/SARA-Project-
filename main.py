e8d322972348d85d095c7a01468b6e0457d59fc2
Your Name <your-email@example.com>
2026-02-16 07:02:13 +0000

    Fixed main.py (pure Python)

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from kivy.uix.button import Button
 
 class SaraApp(App):
     def build(self):
-        # একটি সুন্দর বাটন তৈরি করছি
-        return Button(
-            text='Hello Anif! S.A.R.A. is Running...',
-            font_size='20sp',
-            background_color=(0, 0.5, 1, 1)
-        )
+        return Button(text='Hello Anif! S.A.R.A. is Running...')
 
 if __name__ == '__main__':
     SaraApp().run()
